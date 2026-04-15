26bd21b43d0a89c34a0068edd9070c286f6c619b
Nikolai Voronkov <nikolaivoronkov@gmail.com>
2026-04-15 17:17:49 +0300

    fonts fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import { Cormorant_Garamond } from "next/font/google";
 import Header from "@/app/ui/header/header";
 import Footer from "@/app/ui/footer/footer";
-
-const cormorant = Cormorant_Garamond({
-  subsets: ["latin"],
-  style: "italic",
-  weight: ["300", "400", "600"],
-});
 
 export const metadata: Metadata = {
   title: "Social Massage",
@@ -21,7 +14,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={`${cormorant.className} h-full antialiased`}>
+    <html lang="en" className="h-full antialiased">
       <body className="min-h-full flex flex-col">
         <Header />
         <div>{children}</div>
